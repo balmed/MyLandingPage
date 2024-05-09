9855c54a418b3eede4e0bf747eeccf0dd5681374
balmed <139716899+balmed@users.noreply.github.com>
2024-05-09 22:57:39 +0200

    Update script.js
    modify file js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Display a modal when the "Explore More" button is clicked
+document.querySelector('.btn').addEventListener('click', function() {
+    document.querySelector('.modal').style.display = 'block';
+});
+
+// Close the modal when the close button is clicked
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.modal').style.display = 'none';
+});
+
 // Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
